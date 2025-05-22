a36839ee57d7ef7f167184467f818d0ab85e7ed2
fastenough69 <van.lisenkov@yandex.ru>
2025-05-22 13:20:53 +0300

    do new table market_pos

--- server/database.py
+++ server/database.py
@@ -8,14 +8,6 @@ import abc
 pool = None
 config = {"user": "admin", "password": "856901", "database":"AppMarketAutodetails", "host": "db"}
 
-class Database:
-    def __init__(self):
-        self.pool = None
-
-    async def connect(self):
-        config = {"user": "admin", "password": "856901", "database":"AppMarketAutodetails", "host": "db"}
-        self.pool = await asyncpg.create_pool(**config)
-
 class Table(abc.ABC):
     def __init__(self, name_table: str):
         self.nametable = name_table
@@ -37,36 +29,68 @@ class Table(abc.ABC):
         await pool.close()
 
 class TableUsers(Table):
-    def __init__(self, name_table):
+    def __init__(self, name_table: str="Users"):
         super().__init__(name_table)
 
     async def create_table(self):
-        # db = Database()
-        # await db.connect()
-        # self.__conn = db.pool
-        await super().connect()
+        await self.connect()
         async with pool.acquire() as conn:
-            await conn.execute(""" CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, name TEXT, balance numeric, password bytea ) """)
+            await conn.execute(""" CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, name TEXT, balance numeric, password bytea, admins boolean ) """)
+        await  self._destroy_conn()
 
-    async def insert_into_table(self, name: str ,balance: Decimal, passwd: bytes):
-        async with self.__conn.acquire() as conn:
-            await conn.execute(""" INSERT INTO users (name, balance, password) VALUES($1, $2, $3) """, name, balance, passwd)
+    async def insert_into_table(self, name: str, balance: Decimal, passwd: bytes, admin: bool):
+        await self.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(""" INSERT INTO users (name, balance, password, admins) VALUES($1, $2, $3, $4) """, name, balance, passwd, admin)
+        await self._destroy_conn()
 
     async def get_data(self) -> dict:
-        async with self.__conn.acquire() as conn:
+        await self.connect()
+        async with pool.acquire() as conn:
             dicts = [dict(row) for row in await conn.fetch(""" SELECT (id, name, balance) FROM users """)]
+            await self._destroy_conn()
             return dicts
 
-    def __del__(self):
-        pass
-        
-class TableBasket:
-    def __init__(self):
-        pass
+    async def is_admin(self, id) -> bool:
+        await self.connect()
+        async with pool.acquire() as conn:
+            result = await conn.fetchval(""" SELECT (admins) FROM users WHERE id = $1  """, id)
+            await self._destroy_conn()
+            return {"admin"}
 
-class TableMarketPosition:
-    def __init__(self):
-        pass
+
+# class TableAdmins(Table):
+#     def __init__(self, name_table: str="Admins"):
+#         super().__init__(name_table)
+
+#     async def create_table(self):
+#         await self.connect()
+#         async with pool.acquire() as conn:
+#             await conn.execute(""" CREATE TABLE IF NOT EXISTS  """)
+        
+class TableBasket(Table):
+    def __init__(self, name_table: str="Basket"):
+        super().__init__(name_table)
+
+    async def create_table(self):
+        await self.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(""" CREATE TABLE IF NOT EXISTS basket (id SERIAL PRIMARY KEY, userId NOT NULL REFERENCES users(id)) """)
+
+        
+class TableMarketPosition(Table):
+    def __init__(self, name_table: str="marketPos"):
+        super().__init__(name_table)
+
+    async def create_table(self):
+        await self.connect()
+        async with pool.acquire() as conn:
+            await conn.execute(""" CREATE TABLE IF NOT EXISTS marketPos (id SERIAL PRIMARY KEY, product_name TEXT, cur_count_pos INTEGER, price numeric) """)
+
+    async def insert_into_table(self, product_name: str, count_pos: int, price: Decimal):
+        await self.connect()
+        async with pool.acquire() as conn:
+            await conn.execute() #вот это дописать
 
 class TableOrders:
     def __init__(self):
--- server/main.py
+++ server/main.py
@@ -11,31 +11,27 @@ app = FastAPI()
 async def root():
     return {"text": "privet mir"}
 
-@app.post("/new_pos")
-async def insert_new_pos():
-    pass
-
-@app.get("/get_all")
-async def get_all_pos():
-    pass
-
-@app.get("/create_table_users")
-async def create():
+@app.get("/users/create_table")
+async def create_table_users():
     await user_table.create_table()
     return {"status": "ok"}
 
-@app.post("/insert")
+@app.post("/users/insert")
 async def insert_db(name: str, passwd: str, admin: bool):
     passwd = passwd.encode("utf-8")
     hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
     await user_table.insert_into_table(name, Decimal(0), hashed, admin)
     return {"status": "ok"}
 
-@app.get("/get_users")
+@app.get("/users/get_users")
 async def get_all_users():
     data =  await user_table.get_data()
     return data
 
+@app.get("/users/is_admin")
+async def res_is_admin(id: int):
+    res = await user_table.is_admin(id)
+    return res
 
 if(__name__ == "__main__"):
     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
